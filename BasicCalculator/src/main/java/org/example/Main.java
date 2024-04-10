e66fcc1383c4df50ac602ef75b8faab95de81ccc
osbokirk <Kirk.c.osborne@gmail.com>
2024-04-10 11:56:20 -0500

    implemented while loop for intial floats to prevent invalid user inputs

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -8,67 +8,59 @@ public class Main {
     public static void main(String[] args) {
         //create a scanner class that takes and 2 values
         Scanner userInput = new Scanner(System.in);
-        Float firstNumber = null;
-        Float secondNumber = null;
+        Float firstNumber = (float) 0;
+        Float secondNumber = (float) 0;
+        int x = 0;
         Float sum;
 
         //prompt user for 1st input and save the value
-        System.out.println("Enter the first Number :");
-        while(firstNumber == null) {
+        while (x == 0) {
+            System.out.println("Enter the first Number :");
+            String firstInput = userInput.nextLine();
             try {
-                firstNumber = userInput.nextFloat();
-
-            }catch (InputMismatchException e){
-                System.out.println("hello");
-                break;
-                //continue;
+                firstNumber = Float.parseFloat(firstInput);
+                x++;
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry you did not enter a valid float");
             }
-
-
-                //throw new InputMismatchException("sorry You didn't enter a valid float");
-
-                //firstNumber = userInput.nextFloat();
-
         }
 
-        //prompt user for 2nd input and save the value
-        System.out.println("Enter the second Number :");
-        while(secondNumber == null) {
+        while (x == 1) {
+            System.out.println("Enter the second number :");
+            String secondInput = userInput.nextLine();
+            x++;
             try {
-                secondNumber = userInput.nextFloat();
-            } catch (InputMismatchException e) {
-                throw new InputMismatchException("sorry You didn't enter a valid float");
+                secondNumber = Float.parseFloat(secondInput);
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry you didn't enter a valid float");
             }
         }
 
         //prompt the user for there wanted action && return result
         System.out.println("Possible Calculations:\n    (A)dd\n    (S)ubtract\n    (M)ultiply\n    (D)ivide");
-        userInput.nextLine();
+        //userInput.nextLine();
         String artimitic = userInput.nextLine();
+        String options = "ASMDasmd";
 
-        /*
-        if (artimitic.length() > 0 || ( artimitic.equalsIgnoreCase("A")  ||artimitic.equalsIgnoreCase("D") ||
-                artimitic.equalsIgnoreCase("S") || artimitic.equalsIgnoreCase("M"))){
-            System.out.println("fight");
-        }
-        */
-        if (artimitic.equalsIgnoreCase("A")){
+        if (artimitic.length() == 1 && options.contains(artimitic)) {
+
+            if (artimitic.equalsIgnoreCase("A")) {
             sum = firstNumber + secondNumber;
             System.out.println(sum);
-        }
-        else if(artimitic.equalsIgnoreCase("S")){
+            }
+            else if (artimitic.equalsIgnoreCase("S")) {
             sum = firstNumber - secondNumber;
             System.out.println(sum);
-        }
-        else if(artimitic.equalsIgnoreCase("M")){
+            }
+            else if (artimitic.equalsIgnoreCase("M")) {
             sum = firstNumber * secondNumber;
             System.out.println(sum);
-        }
-        else if(artimitic.equalsIgnoreCase("D")){
-            sum = firstNumber/secondNumber;
+            }
+            else if (artimitic.equalsIgnoreCase("D")) {
+            sum = firstNumber / secondNumber;
             System.out.println(sum);
-        }
-        else{
+            }
+        } else{
             System.out.println("Sorry invalid option");
         }
     }
