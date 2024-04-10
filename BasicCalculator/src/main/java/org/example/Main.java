5ff9d145a5248ddd6ed4184e874e0731a4492e3e
osbokirk <Kirk.c.osborne@gmail.com>
2024-04-09 22:28:40 -0500

    while loop not working

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -1,43 +1,74 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //create a scanner class that takes and 2 values
         Scanner userInput = new Scanner(System.in);
-        float firstNumber;
-        float secondNumber;
-        float sum;
+        Float firstNumber = null;
+        Float secondNumber = null;
+        Float sum;
 
         //prompt user for 1st input and save the value
         System.out.println("Enter the first Number :");
-        firstNumber = userInput.nextFloat();
+        while(firstNumber == null) {
+            try {
+                firstNumber = userInput.nextFloat();
+
+            }catch (InputMismatchException e){
+                System.out.println("hello");
+                break;
+                //continue;
+            }
+
+
+                //throw new InputMismatchException("sorry You didn't enter a valid float");
+
+                //firstNumber = userInput.nextFloat();
+
+        }
+
         //prompt user for 2nd input and save the value
         System.out.println("Enter the second Number :");
-        secondNumber = userInput.nextFloat();
-
-
+        while(secondNumber == null) {
+            try {
+                secondNumber = userInput.nextFloat();
+            } catch (InputMismatchException e) {
+                throw new InputMismatchException("sorry You didn't enter a valid float");
+            }
+        }
 
         //prompt the user for there wanted action && return result
-        System.out.println("Possible Calculations:\n    (A)dd\n (S)ubtract\n    (M)ultiply\n    (D)ivide");
+        System.out.println("Possible Calculations:\n    (A)dd\n    (S)ubtract\n    (M)ultiply\n    (D)ivide");
         userInput.nextLine();
         String artimitic = userInput.nextLine();
 
-
+        /*
+        if (artimitic.length() > 0 || ( artimitic.equalsIgnoreCase("A")  ||artimitic.equalsIgnoreCase("D") ||
+                artimitic.equalsIgnoreCase("S") || artimitic.equalsIgnoreCase("M"))){
+            System.out.println("fight");
+        }
+        */
         if (artimitic.equalsIgnoreCase("A")){
             sum = firstNumber + secondNumber;
             System.out.println(sum);
-        }else if(artimitic.equalsIgnoreCase("S")){
+        }
+        else if(artimitic.equalsIgnoreCase("S")){
             sum = firstNumber - secondNumber;
             System.out.println(sum);
-        }else if(artimitic.equalsIgnoreCase("M")){
+        }
+        else if(artimitic.equalsIgnoreCase("M")){
             sum = firstNumber * secondNumber;
             System.out.println(sum);
-        }else if(artimitic.equalsIgnoreCase("D")){
+        }
+        else if(artimitic.equalsIgnoreCase("D")){
             sum = firstNumber/secondNumber;
             System.out.println(sum);
-        }else{
+        }
+        else{
             System.out.println("Sorry invalid option");
         }
     }
